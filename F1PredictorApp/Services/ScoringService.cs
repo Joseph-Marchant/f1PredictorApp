8c79c3912dae7613e0823a220fff1320b28354a5
Joseph Marchant <joseph.marchant@outlook.com>
2023-12-23 15:05:38 +0000

    New scoring

---
+++ F1PredictorApp/Services/ScoringService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1PredictorApp.Services;
+
+public class ScoringService
+{
+    // 1 point per correctly guessed driver
+    // n point(s) for n dirvers in correct position
+    // 1 point for predicted driver getting fastest lap
+    // n point(s) for predicted driver finishing 4+n positions ahead of starting
+    // -1 point
+}
