b48b2d8f1ba2da199918f861f9a974f178d1da8a
Joseph Marchant <joseph.marchant@outlook.com>
2025-03-15 18:14:25 +0000

    Moved away from absolute file paths

--- F1PredictionTracker/F1PredictionTracker.Adapters.FileStorage/FileStorageConfig.cs
+++ F1PredictionTracker/F1PredictionTracker.Adapters.FileStorage/FileStorageConfig.cs
@@ -1,10 +1,18 @@
+using System.Reflection;
+
 namespace F1PredictionTracker.Adapters.FileStorage;
 
 public class FileStorageConfig
 {
-    public string PredictionsFilePath => @"C:\Users\jwf_m\Documents\Code\F1PredictorApp\F1PredictionTracker\F1PredictionTracker.Adapters.FileStorage\StoredFiles\predictions.json";
+    public string PredictionsFilePath => GetFilePath("predictions.json");
     
-    public string PredictionsStandingsFilePath => @"C:\Users\jwf_m\Documents\Code\F1PredictorApp\F1PredictionTracker\F1PredictionTracker.Adapters.FileStorage\StoredFiles\predictionsStandings.json";
+    public string PredictionsStandingsFilePath => GetFilePath("predictionsStandings.json");
     
-    public string StateFilePath => @"C:\Users\jwf_m\Documents\Code\F1PredictorApp\F1PredictionTracker\F1PredictionTracker.Adapters.FileStorage\StoredFiles\state.json";
+    public string StateFilePath => GetFilePath("state.json");
+
+    private string GetFilePath(string fileName)
+    {
+        var directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? throw new NullReferenceException("Unable to get executing assembly location");
+        return Path.Combine(directory, "StoredFiles", fileName);
+    }
 }
