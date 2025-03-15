c9c232edb627e9b67ab947a63dbcc0d408782e49
Joseph Marchant <joseph.marchant@outlook.com>
2025-03-15 09:44:54 +0000

    Fixed file paths

--- F1PredictionTracker/F1PredictionTracker.Adapters.FileStorage/FileStorageConfig.cs
+++ F1PredictionTracker/F1PredictionTracker.Adapters.FileStorage/FileStorageConfig.cs
@@ -2,9 +2,9 @@ namespace F1PredictionTracker.Adapters.FileStorage;
 
 public class FileStorageConfig
 {
-    public string PredictionsFilePath => @"C:\Users\jwf_m\Documents\Code\F1PredictionTracker\F1PredictionTracker\F1PredictionTracker.Adapters.FileStorage\StoredFiles\predictions.json";
+    public string PredictionsFilePath => @"C:\Users\jwf_m\Documents\Code\F1PredictorApp\F1PredictionTracker\F1PredictionTracker.Adapters.FileStorage\StoredFiles\predictions.json";
     
-    public string PredictionsStandingsFilePath => @"C:\Users\jwf_m\Documents\Code\F1PredictionTracker\F1PredictionTracker\F1PredictionTracker.Adapters.FileStorage\StoredFiles\predictionsStandings.json";
+    public string PredictionsStandingsFilePath => @"C:\Users\jwf_m\Documents\Code\F1PredictorApp\F1PredictionTracker\F1PredictionTracker.Adapters.FileStorage\StoredFiles\predictionsStandings.json";
     
-    public string StateFilePath => @"C:\Users\jwf_m\Documents\Code\F1PredictionTracker\F1PredictionTracker\F1PredictionTracker.Adapters.FileStorage\StoredFiles\state.json";
+    public string StateFilePath => @"C:\Users\jwf_m\Documents\Code\F1PredictorApp\F1PredictionTracker\F1PredictionTracker.Adapters.FileStorage\StoredFiles\state.json";
 }
